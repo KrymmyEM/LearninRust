3277b9d61e05cb7a15cf6d8e8a713391db35a141
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2025-01-25 20:19:14 +0300

    [UP] ch05>Rectangle+{n can_hold_my}

--- book/ch05_struct/src/main.rs
+++ book/ch05_struct/src/main.rs
@@ -9,6 +9,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold_my(&self, rect: &Rectangle) -> bool {
+        (self.width <= rect.width) & (self.height <= rect.height)
+    }
 }
 
 fn area(width: u32, height: u32) -> u32 {
@@ -34,4 +38,21 @@ fn main() {
     println!("The area of the rectangle is {} square pixels.", area_rectangle(&rect));
     println!("The area of the rectangle is {} square pixels.", rect.area());
 
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    println!("Can rect1 hold rect2? {}", rect1.can_hold_my(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold_my(&rect3));
+    //println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    //println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
