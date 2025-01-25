ac38745a2af22ad46dd25579655422718d2960bc
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2025-01-25 20:01:48 +0300

    [UP] ch05 Struct

--- book/ch05_struct/src/main.rs
+++ book/ch05_struct/src/main.rs
@@ -1,4 +1,7 @@
-
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
 fn area(width: u32, height: u32) -> u32 {
     width * height
@@ -8,7 +11,17 @@ fn area_tuple(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
 }
 
+fn area_rectangle(rect: &Rectangle) -> u32 {
+    rect.width * rect.height
+}
+
 fn main() {
     println!("The area of the rectangle is {} square pixels.", area(30, 50));
     println!("The area of the rectangle is {} square pixels.", area_tuple((30, 50)));
+    let rect: Rectangle = Rectangle{
+        width: 100,
+        height: 200
+    };
+    println!("The area of the rectangle is {} square pixels.", area_rectangle(&rect));
+
 }
