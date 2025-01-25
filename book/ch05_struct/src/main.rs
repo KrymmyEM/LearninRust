8866c6f6dd27c487e0f594d6a428acbd361307a3
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2025-01-25 19:57:21 +0300

    [UP] ch05

--- book/ch05_struct/src/main.rs
+++ book/ch05_struct/src/main.rs
@@ -1,3 +1,13 @@
+
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area_tuple(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("The area of the rectangle is {} square pixels.", area(30, 50));
 }
