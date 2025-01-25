62f520db668cedcd704c0e0a817a3d85ac6e4fc6
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2025-01-25 18:48:28 +0300

    [UP] ch04_03 Solution problem

--- book/ownership_4/ch04_03_slices/src/main.rs
+++ book/ownership_4/ch04_03_slices/src/main.rs
@@ -1,3 +1,20 @@
+
+// Here's a little programming problem: write a function that takes a string of words,
+// separated by spaces, and returns the first word it finds in that string.
+// If the function doesn't find a space in the string, the entire string must consist of a single word,
+// so the entire string should be returned.
+
+
+// fast solution
+fn firts_word_my(s: &String) -> String {
+    for word in s.split(' '){
+        return  String::from(word);
+    }
+    s.clone()
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let firts_word = firts_word_my(&String::from("Hello"));
+    println!("{}", firts_word)
 }
