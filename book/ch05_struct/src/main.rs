c6a774bf02cd2bcae74da751967d2620f4f33aac
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2025-01-25 20:04:33 +0300

    [UP] ch05 add debug derive

--- book/ch05_struct/src/main.rs
+++ book/ch05_struct/src/main.rs
@@ -1,3 +1,5 @@
+
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -22,6 +24,7 @@ fn main() {
         width: 100,
         height: 200
     };
+    println!("Rect: {:?}", rect);
     println!("The area of the rectangle is {} square pixels.", area_rectangle(&rect));
 
 }
