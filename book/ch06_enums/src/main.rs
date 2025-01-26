e2b39162163a6ff17a5849403c133632692482e0
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2025-01-26 12:13:49 +0300

    [UP] ch06+impl[+Action, +Character]

--- book/ch06_enums/src/main.rs
+++ book/ch06_enums/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, str::FromStr};
 
 
 enum Message {
@@ -6,7 +6,10 @@ enum Message {
     Exit,
     Punch(Character, i32),
     Quit,
-    Move(Position),
+    Move{
+        x: i32,
+        y: i32
+    },
     Write(String),
     ChangeColor(i32, i32, i32),
 }
@@ -17,12 +20,53 @@ struct Position{
 }
 
 struct Character {
+    id: String,
     health: i32,
     position: Position
 }
 
+impl Character {
+    fn new(id: String, health: i32, position: Position) -> Self {
+        Self {
+            id,
+            health,
+            position
+        }        
+    }
+    
+}
+
 struct Actions {
-    actions: HashMap<Character, Vec<Message>>,
+    actions: HashMap<String, Vec<Message>>,
+}
+
+impl Actions {
+    fn new() -> Self {
+        Self{
+            actions: HashMap::new()
+        }
+        
+    }
+    fn add_character(&mut self, character: &Character) {
+        self.actions.insert(character.id.clone(), Vec::new());
+    }
+
+    fn add_action(&mut self, character: &Character, message: Message){
+        let mut messages = self.actions.get_mut(&character.id).expect("Key not found");
+        messages.push(message);
+    }
+
+    fn clear_actons(&mut self){
+        for value in self.actions.values_mut(){
+            value.clear();
+        }
+
+    }
+
+    fn clear(&mut self){
+        self.actions.clear();
+    }
+    
 }
 
 fn main() {
