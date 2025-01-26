5fab8087210ec69269fbdbd0d5a938ce0d3f8c80
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2025-01-26 11:36:46 +0300

    [UP] ch06 + enum Message;+scruct[Position, Character, Actions]

--- book/ch06_enums/src/main.rs
+++ book/ch06_enums/src/main.rs
@@ -1,3 +1,30 @@
+use std::collections::HashMap;
+
+
+enum Message {
+    Enter,
+    Exit,
+    Punch(Character, i32),
+    Quit,
+    Move(Position),
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+struct Position{
+    x: i32,
+    y: i32
+}
+
+struct Character {
+    health: i32,
+    position: Position
+}
+
+struct Actions {
+    actions: HashMap<Character, Vec<Message>>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
