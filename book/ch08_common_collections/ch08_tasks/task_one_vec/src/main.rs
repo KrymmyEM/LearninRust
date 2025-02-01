61f27a162a7d2cf06539da2139b79e30964a9863
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2025-02-01 17:16:30 +0300

    [UP] ch08/task_one_vec main.rs start

--- book/ch08_common_collections/ch08_tasks/task_one_vec/src/main.rs
+++ book/ch08_common_collections/ch08_tasks/task_one_vec/src/main.rs
@@ -1,3 +1,23 @@
+
+// Sort algoritm
+fn insert_bubble_sort(mut vector: Vec<i32>) -> Vec<i32>{
+    if !vector.is_empty(){
+        let size: usize = vector.len();
+        let even: bool = (size % 2) == 0;
+        for i in 0..size{
+            let last_index: usize = size - 1 - i;
+            if even && last_index == i {
+                break;
+            }
+            //let i_value: i32 = vector.swap_remove(i);
+            //let last_value: i32 = vector.pop(last_index);
+
+        }
+    }
+
+    vector
+}
+
 fn main() {
     println!("Hello, world!");
 }
