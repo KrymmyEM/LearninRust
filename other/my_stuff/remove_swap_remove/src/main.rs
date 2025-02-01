feb0b76d691dc7fd34c7feec48db2237f26fec04
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2025-02-01 17:48:14 +0300

    [UP] remove_swap_remove ADD benchmark

--- other/my_stuff/remove_swap_remove/src/main.rs
+++ other/my_stuff/remove_swap_remove/src/main.rs
@@ -1,3 +1,13 @@
+use std::time::Instant;
+use rand::Rng;
+
+fn generate_random_vector(size: usize) -> Vec<i32> {
+    let mut rng = rand::rng();
+    (0..size).map(|_| rng.random_range(-100..=100)).collect()
+}
+
+
+
 fn remove(vector: &mut Vec<i32>, index: usize) {
     if !vector.is_empty(){
         let size = vector.len();
@@ -31,12 +41,37 @@ fn swap_remove(vector: &mut Vec<i32>, index: usize) {
     }
 }
 
-fn main() {
-    let mut data = vec![0,2,3,4,5,6];
-    println!("{:?}", data);
-    //remove(&mut data, 1);
-    println!("{:?}", data);
+fn test_base_remove(mut data: Vec<i32>) {
+    //println!("{:?}", data);
+    data.remove(1);
+    //println!("{:?}", data);
+    data.swap_remove(5);
+    //println!("{:?}", data);
+}
+
+fn test_handmade_remove(mut data: Vec<i32>) {
+    //println!("{:?}", data);
+    remove(&mut data, 1);
+    //println!("{:?}", data);
     swap_remove(&mut data, 5);
-    println!("{:?}", data);
+    //println!("{:?}", data);
+}
+
+fn benchmark<F>(func: F, mut data: Vec<i32>, label: &str)
+where
+    F: Fn(Vec<i32>),
+{
+    let start = Instant::now();
+    let _ = func(data);
+    let duration = start.elapsed();
+    println!("{} took: {:?}", label, duration);
+}
+
+// With prints base ralisation slower than my handmade
+// idk why. But without prints my handmade is slower
+fn main() {
+    let mut data = generate_random_vector(100);
+    benchmark(test_base_remove, data.clone(), "base remove");
+    benchmark(test_handmade_remove, data.clone(), "handmade remove");
    
 }
